b5a57575b279922fd215590f6207e7eac9faa36a
Michael Truong <truong.minh.michael@gmail.com>
2022-09-03 18:45:54 -0700

    handle CORS error

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ const Auth = require('./models/auth.model');
 
 dbConnect()
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+  
+
 // body parser configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
