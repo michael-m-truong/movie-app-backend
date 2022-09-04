c5bcf29f37bcf4d62a06dbfbfb1031d0d0109cb0
Michael Truong <truong.minh.michael@gmail.com>
2022-09-03 19:25:22 -0700

    testing pipeline

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/auth', authRouter)
 
 app.get("/", (request, response) => {
-  response.json({ message: "Hello World! API works!" });
+  response.json({ message: "Hello World! API works! TESTING UPDATE" });
 });
 
 module.exports = app;
