28e0c0944fb9323541c1b572fd5e5527172dd87b
Michael Truong <truong.minh.michael@gmail.com>
2022-09-04 01:34:26 -0700

    testing pipeline

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -60,7 +60,7 @@ describe('auth test', () => {
             .post("/auth/login/")
             .set('Accept', 'application/json')
             .send({username: "Test123", password: "abc123"})
-            .expect(400)
+            .expect(401)
             .expect("Content-Type", /json/)
     }) 
 })
