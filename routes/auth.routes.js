91e2f62dc8cd292a99ca0371c82cd6a6e87196c8
Michael Truong <truong.minh.michael@gmail.com>
2022-12-28 14:19:14 -0800

    added some swagger docs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,8 +3,96 @@ const router = express.Router();
 const AuthController = require('../controllers/auth.controller')
 
 ///{controller}/{action}
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - finished
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The title of your book
+ *         author:
+ *           type: string
+ *           description: The book author
+ *         finished:
+ *           type: boolean
+ *           description: Whether you have finished reading the book
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the book was added
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ *         finished: false
+ *         createdAt: 2020-03-10T04:05:06.157Z
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: The books managing API
+ * /auth/register:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
+ *
+ */
 router.post('/register', AuthController.register)
 
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: The books managing API
+ * /auth/login:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
+ *
+ */
 router.post('/login', AuthController.login)
 
 module.exports = router;
