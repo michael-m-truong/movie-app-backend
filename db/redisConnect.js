2a8db189ff98fb9388617f810123ec638f99da69
Michael Truong <truong.minh.michael@gmail.com>
2023-06-26 21:16:40 -0700

    add try catch around singleton

--- db/redisConnect.js
+++ db/redisConnect.js
@@ -4,16 +4,21 @@ require('dotenv').config();
 let redisInstance = null;
 
 function redisConnect() {
-  if (!redisInstance) {
-    redisInstance = new Redis(process.env.REDIS_INTERNAL);
+  try {
+    if (!redisInstance) {
+      redisInstance = new Redis(process.env.REDIS_INTERNAL);
 
-    redisInstance.on('connect', () => {
-      console.log('Successfully connected to Redis server!');
-    });
+      redisInstance.on('connect', () => {
+        console.log('Successfully connected to Redis server!');
+      });
 
-    redisInstance.on('error', (error) => {
-      console.error('Error connecting to Redis server:', error);
-    });
+      redisInstance.on('error', (error) => {
+        console.error('Error connecting to Redis server:', error);
+      });
+    }
+  }
+  catch (error) {
+    console.log(error)
   }
 
   return redisInstance;
